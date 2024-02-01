a4adddf1b0824239ca67cbd54fc271f6bec8e32d
August Semrau Andersen <augu0093@gmail.com>
2024-02-01 16:32:57 +0100

    fixed project structure problem

---
+++ src/thesis2024/datamodules/make_dataset.py
@@ -0,0 +1,126 @@
+"""Create the dataset for the thesis.
+
+Making the dataset for the thesis, by loading datasources (currently PDFs)
+and creating a persistent Chroma vector store from them.
+"""
+
+import os
+import openai
+from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+
+
+
+
+def load_peristent_chroma_store(openai_embedding):
+    """Load a persistent Chroma vector store.
+
+    :param persist_path: Path to the persistent Chroma vector store.
+    :return: Chroma vector store.
+    """
+    ## Initiate embedding function
+    if openai_embedding:
+        embedding_func = OpenAIEmbeddings()
+    else:
+        embedding_func = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    persist_dir = os.path.join(os.getcwd(), 'data/processed/chroma')
+
+    return Chroma(persist_directory=persist_dir, embedding_function=embedding_func)
+
+
+
+def create_persistent_chroma_store(openai_embedding, chunk_size, chunk_overlap):
+    """Create Vector Store.
+
+    Create a persistent Chroma vector store from PDF documents in a directory.
+    :param directory: Path to the directory containing PDF documents.
+    :param store_path: Path to save the persistent Chroma vector store.
+    :param chunk_size: Size of text chunks for each document.
+    :param chunk_overlap: Overlap between consecutive text chunks.
+    :return: None
+    """
+    data_dir = os.path.join(os.getcwd(), 'data/raw/DLAI_CWYDcourse')
+    persist_dir = os.path.join(os.getcwd(), 'data/processed/chroma')
+
+    # Process each PDF file in the directory
+    all_docs = []
+    for filename in os.listdir(data_dir):
+        if filename.endswith('.pdf'):
+            file_path = os.path.join(data_dir, filename)
+
+            # Load PDF document
+            loader = PyPDFLoader(file_path)
+            documents = loader.load()
+
+            # Split documents into chunks
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=chunk_size,
+                chunk_overlap=chunk_overlap,
+                separators=["\n\n", "\n", r"(?<=\. )", " ", ""]
+            )
+            docs = text_splitter.split_documents(documents)
+            print(f"{len(docs)} chunks created from {filename}")
+            all_docs.extend(docs)
+
+    if openai_embedding:
+        embedding_func = OpenAIEmbeddings()
+    else:
+        embedding_func = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+    # Save the Chroma vector store persistently
+    Chroma.from_documents(all_docs, embedding_func, persist_directory=persist_dir)
+    return print(f"Chroma vector store created and saved at {persist_dir}")
+
+
+
+if __name__ == '__main__':
+
+    ## Retrieve OpenAI API key from environment variable
+    openai.api_key  = os.environ['OPENAI_API_KEY']
+
+    # Get the data and process it
+    create_persistent_chroma_store(openai_embedding=True,
+                                   chunk_size=1000,
+                                   chunk_overlap=150)
+    pass
+
+
+
+
+
+
+
+
+
+
+
+# def merge_hyphenated_words(text: str) -> str:
+#     """Merge hyphenated words."""
+#     return re.sub(r"(\w)-\n(\w)", r"\1\2", text)
+
+# def fix_newlines(text: str) -> str:
+#     """Fix newlines."""
+#     return re.sub(r"(?<!\n)\n(?!\n)", " ", text)
+
+# def remove_multiple_newlines(text: str) -> str:
+#     """Remove multiple newlines."""
+#     return re.sub(r"\n{2,}", "\n", text)
+
+
+# def clean_text(pages: List[Tuple[int, str]], cleaning_functions: List[Callable[[str], str]]) -> List[Tuple[int, str]]:
+#     """Clean text.
+
+#     :param pages: List of tuples of page number and text.
+#     :param cleaning_functions: List of cleaning functions.
+#     :return: List of tuples of page number and text.
+#     """
+#     cleaned_pages = []
+#     for page_num, text in pages:
+#         for cleaning_function in cleaning_functions:
+#             text = cleaning_function(text)
+#         cleaned_pages.append((page_num, text))
+#     return cleaned_pages
--- src/thesis2024/models/chatbot_v1.py
+++ src/thesis2024/models/chatbot_v1.py
@@ -5,17 +5,11 @@ Load the persistent Chroma vector store and create a chatbot using OpenAI's API.
 
 import os
 import openai
-from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 
-def load_peristent_chroma_store(embedding_function,
-                                persist_dir='data/processed/chroma'):
-    """Load a persistent Chroma vector store.
+from thesis2024.datamodules.make_dataset import load_peristent_chroma_store
 
-    :param persist_path: Path to the persistent Chroma vector store.
-    :return: Chroma vector store.
-    """
-    return Chroma(persist_directory=persist_dir, embedding_function=embedding_function)
+
+
 
 
 
@@ -24,20 +18,17 @@ if __name__ == '__main__':
     ## Retrieve OpenAI API key from environment variable
     openai.api_key  = os.environ['OPENAI_API_KEY']
 
-    ## Initiate embedding function
-    # embedding_func = OpenAIEmbeddings()
-    embedding_func = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
-    persist_path = os.path.join(os.getcwd(), 'data/processed/chroma')
+
 
     ## Load persistent Chroma vector store
-    chroma_instance = load_peristent_chroma_store(embedding_function=embedding_func, persist_dir=persist_path)
+    chroma_instance = load_peristent_chroma_store(openai_embedding=True)
 
     ## Query the Chroma vector store
-    query = "Piss off ghost!"
+    query = "I like AI!"
     # query = 'So let me show you a video now. Load the big screen, please. So Ill show you a video now that was from Dean Pomerleau at some work he did at Carnegie Mellon on applied supervised learning to get a car to drive itself. This is work on a vehicle known as Alvin.  It was done sort of about 15 years ago, and I think it was a very elegant example of the sorts of things you can get supervised or any algorithms to do.'
     search_results = chroma_instance.similarity_search(query, k=3)
     # search_results = chroma_instance.max_marginal_relevance_search(query, k=3, fetch_k=5)
-
+    print(search_results)
 
 
