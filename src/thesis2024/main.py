b041051a8306f7340905d15e6032f40268b3d2ce
August Semrau Andersen <augu0093@gmail.com>
2024-03-08 14:07:53 +0100

    redundant changes for now

--- src/thesis2024/main.py
+++ src/thesis2024/main.py
@@ -1,15 +1,11 @@
 """Main script for the thesis project."""
 
-import os
 import sys
+import pprint
 
-## Langchain imports
-from langgraph.graph import END, StateGraph
 
 ## Local imports
-from thesis2024.utils import GraphState
-from thesis2024.datamodules.crag import CragNodes
-from thesis2024.datamodules.retrieval_agent import RetrievalAgent
+from thesis2024.datamodules.crag import Crag
 
 
 
@@ -18,45 +14,33 @@ from thesis2024.datamodules.retrieval_agent import RetrievalAgent
 
 
 
-### Function for building graph
-def build_rag_graph(node_class):
-    """Build the graph for the CRAG model."""
-    workflow = StateGraph(GraphState)
 
-    # Define the nodes
-    workflow.add_node("retrieve", node_class.retrieve)  # retrieve
-    workflow.add_node("grade_documents", node_class.grade_documents)  # grade documents
-    workflow.add_node("generate", node_class.generate)  # generatae
-    workflow.add_node("transform_query", node_class.transform_query)  # transform_query
-    workflow.add_node("web_search", node_class.web_search)  # web search
-
-    # Build graph
-    workflow.set_entry_point("retrieve")
-    workflow.add_edge("retrieve", "grade_documents")
-    workflow.add_conditional_edges(
-        "grade_documents",
-        node_class.decide_to_generate,
-        {
-            "transform_query": "transform_query",
-            "generate": "generate",
-        },
-    )
-    workflow.add_edge("transform_query", "web_search")
-    workflow.add_edge("web_search", "generate")
-    workflow.add_edge("generate", END)
-
-    # Compile
-    app = workflow.compile()
-    return app
 
 
 
 
 if __name__ == "__main__":
+
+
+    
     # Build the graph
-    app = build_rag_graph()
+    crag_class = Crag()
+    app = crag_class.build_rag_graph()
     print(app)
     # Run the graph
-    app.run()
+        # Run
+    inputs = {"keys": {"question": "Who is the teacher of the machine learning course, and how come the highest mountains are located in asia?"}}
+    for output in app.stream(inputs):
+        for key, value in output.items():
+            # Node
+            pprint.pprint(f"Node '{key}':")
+            # Optional: print full state at each node
+            # pprint.pprint(value["keys"], indent=2, width=80, depth=None)
+        pprint.pprint("\n---\n")
+
+    # Final generation
+    pprint.pprint(value["keys"]["generation"])
+
     print("Graph run complete.")
-    sys.exit(0)
+    # sys.exit(0)
+
