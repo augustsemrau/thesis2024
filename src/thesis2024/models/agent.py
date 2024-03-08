3a734abb8a0635959b5c3f5983a731188f72ffc3
August Semrau Andersen <augu0093@gmail.com>
2024-03-08 14:07:20 +0100

    implementing simple QA model as baseline

--- src/thesis2024/models/agent.py
+++ src/thesis2024/models/agent.py
@@ -1,9 +1,21 @@
 """Functions for the agent model."""
 
+# LangChain imports
+from langchain import hub
+from langchain_core.output_parsers import StrOutputParser
+
+
+
+
+# Local imports
+from thesis2024.utils import GraphState
+
+
+
+
 
 from langchain_openai import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
-
 def create_agent_chain():
     """Create agent chain.
 
@@ -14,4 +26,78 @@ def create_agent_chain():
     llm = ChatOpenAI(model_name=model_name)
     chain = load_qa_chain(llm, chain_type="stuff")
     return chain
+
+
+
+
+class SimpleAgeneicAgent:
+    """SimpleAgeneicAgent class.
+
+    A simple agent that uses the LangChain library to answer questions.
+    """
+
+    def __init__(self,
+                generate_model: str="gpt-3.5-turbo"):
+        """Initialize the SimpleAgeneicAgent class."""
+        self.generate_model = generate_model
+        return None
+
+
+    def generate(self, state):
+        """Node Function: Generate answer.
+
+        Args:
+        ----
+            state (dict): The current graph state
+
+        Returns:
+        -------
+            state (dict): New key added to state, generation, that contains LLM generation
+
+        """
+        print("---GENERATE---")
+        state_dict = state["keys"]
+        question = state_dict["question"]
+        documents = state_dict["documents"]
+
+        # Prompt
+        # prompt = hub.pull("rlm/rag-prompt")
+        prompt "You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
+
+Question: {question} 
+
+Context: {context} 
+
+Answer:"
+
+        # LLM
+        llm = ChatOpenAI(model_name=self.generate_model, temperature=0, streaming=True)
+
+        # Post-processing
+        def format_docs(docs):
+            return "\n\n".join(doc.page_content for doc in docs)
+
+        # Chain
+        rag_chain = prompt | llm | StrOutputParser()
+
+        # Run
+        generation = rag_chain.invoke({"context": documents, "question": question})
+        return {
+            "keys": {"documents": documents, "question": question, "generation": generation}
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
