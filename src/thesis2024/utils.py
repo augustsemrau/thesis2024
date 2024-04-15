df525ae967cbaf2af1e5b8d9a3d83d21d25a98ae
August Semrau Andersen <augu0093@gmail.com>
2024-04-15 09:37:07 +0200

    moved init of end and langsmith to utils module

---
+++ src/thesis2024/utils.py
@@ -0,0 +1,39 @@
+"""Utility functions for the thesis project."""
+
+import os
+import getpass
+
+from langchain_openai import ChatOpenAI
+
+def init_llm_langsmith(llm_key = 3, temp = 0.5, langsmith_name: str = ""):
+    """Initialize the LLM model and LangSmith tracing.
+
+    Args:
+    ----
+    llm_key (int): The LLM model designation.
+    temp (float): The temperature to use in the LLM model.
+    langsmith_name (str): The name of the LangSmith project.
+
+    Returns:
+    -------
+    ChatOpenAI: LLM model.
+
+    """
+    # Set environment variables
+    def _set_if_undefined(var: str):
+        if not os.environ.get(var):
+            os.environ[var] = getpass(f"Please provide your {var}")
+    _set_if_undefined("OPENAI_API_KEY")
+    _set_if_undefined("LANGCHAIN_API_KEY")
+
+    # Add tracing in LangSmith.
+    os.environ["LANGCHAIN_TRACING_V2"] = "true"
+    if llm_key == 3:
+        llm_ver = "gpt-3.5-turbo-0125"
+    elif llm_key == 4:
+        llm_ver = "gpt-4-0125-preview"
+    os.environ["LANGCHAIN_PROJECT"] = llm_ver + "Temp: " + str(temp) + " " + langsmith_name
+
+    llm_model = ChatOpenAI(model_name=llm_ver, temperature=temp)
+    return llm_model
+
