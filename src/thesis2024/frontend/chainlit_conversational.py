25836913db0a8f4793178d3d53f28e2e10faabcc
August Semrau Andersen <augu0093@gmail.com>
2024-04-15 08:58:14 +0200

    ran frontend

--- src/thesis2024/frontend/chainlit_conversational.py
+++ src/thesis2024/frontend/chainlit_conversational.py
@@ -8,6 +8,10 @@ import chainlit as cl
 import os
 import getpass
 
+from langchain.chains import LLMChain
+from langchain.chat_models import AzureChatOpenAI
+from dotenv import load_dotenv
+
 
 
 def init_llm_langsmith(llm_key = 3):
@@ -32,41 +36,87 @@ def init_llm_langsmith(llm_key = 3):
 llm_ver = init_llm_langsmith(llm_key=3)
 
 
-cb = cl.AsyncLangchainCallbackHandler(stream_final_answer=True)
-config = RunnableConfig(callbacks=[cb])
-result = await agent.ainvoke(input, config=config)
 
-model = ChatOpenAI(model_name=llm_ver, temperature=0, streaming=True)
 
+
+# Load environment variables
+load_dotenv('.env')
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+    ("system", 
+    """
+    You are a friendly assistant that answers user's question about astronomy.
+    If the user's question is not about these topics, 
+    respond with "Uh-oh! I do not have the information to answer your question. Ask me about Space, Planets and Stars!".
+    """
+    ),
+    ("user", "{question}\n"),
+    ]
+)
 
 @cl.on_chat_start
-async def on_chat_start():
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
-            ),
-            ("human", "{question}"),
-        ]
+def main():
+    # Instantiate required classes for the user session
+    llm_chat = AzureChatOpenAI(
+            deployment_name=os.getenv("OPENAI_CHAT_MODEL"),
+            temperature=0.0,
+            max_tokens=100
     )
-    runnable = prompt | model | StrOutputParser()
-    cl.user_session.set("runnable", runnable)
+    llm_chain = LLMChain(prompt=prompt_template, llm=llm_chat, verbose=True)
+
+    # Store the chain in the user session for reusability
+    cl.user_session.set("llm_chain", llm_chain)
 
 
 @cl.on_message
-async def on_message(message: cl.Message):
-    runnable = cl.user_session.get("runnable")  # type: Runnable
-
-    msg = cl.Message(content="")
-
-    async for chunk in runnable.astream(
-        {"question": message.content},
-        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
-    ):
-        await msg.stream_token(chunk)
-
-    await msg.send()
+async def main(message: str):
+    # Retrieve the chain from the user session
+    llm_chain = cl.user_session.get("llm_chain")
+    # Call the chain asynchronously
+    res = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    await cl.Message(content=res["text"]).send()
 
 
 ## chainlit run chainlit_conversational.py -w
+
+
+
+
+
+
+# cb = cl.AsyncLangchainCallbackHandler(stream_final_answer=True)
+# config = RunnableConfig(callbacks=[cb])
+# result = await agent.ainvoke(input, config=config)
+
+# model = ChatOpenAI(model_name=llm_ver, temperature=0, streaming=True)
+
+
+# @cl.on_chat_start
+# async def on_chat_start():
+#     prompt = ChatPromptTemplate.from_messages(
+#         [
+#             (
+#                 "system",
+#                 "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+#             ),
+#             ("human", "{question}"),
+#         ]
+#     )
+#     runnable = prompt | model | StrOutputParser()
+#     cl.user_session.set("runnable", runnable)
+
+
+# @cl.on_message
+# async def on_message(message: cl.Message):
+#     runnable = cl.user_session.get("runnable")  # type: Runnable
+
+#     msg = cl.Message(content="")
+
+#     async for chunk in runnable.astream(
+#         {"question": message.content},
+#         config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+#     ):
+#         await msg.stream_token(chunk)
+
+#     await msg.send()
