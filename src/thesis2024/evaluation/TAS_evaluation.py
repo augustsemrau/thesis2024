79f2c19c7a209f2c2f3723ec5085a1023588dd14
August Semrau Andersen <augu0093@gmail.com>
2024-04-19 15:17:13 +0200

    Aval almost works

--- src/thesis2024/evaluation/TAS_evaluation.py
+++ src/thesis2024/evaluation/TAS_evaluation.py
@@ -4,26 +4,15 @@
 from langchain import chat_models, prompts, smith, hub
 from langchain.schema import output_parser
 from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
 
 # Langsmith imports
-import langsmith
 from langsmith.evaluation import evaluate
 from langsmith.schemas import Run, Example
-from langsmith.wrappers import wrap_openai
 
-import openai
-
+# Local imports
 from thesis2024.utils import init_llm_langsmith
 
-import uuid
-
-
-
-
-
-
-uid = uuid.uuid4()
-client = langsmith.Client()
 
 
 class OtherEvaluationMetrics:
@@ -40,22 +29,25 @@ class OtherEvaluationMetrics:
             return {"key": "is_answered" , "score": 0}
         else:
             return {"key": "is_answered" , "score": 1}
-
     def conversation_length(self, run: Run, example: Example) -> dict:
         """Check the length of the conversation."""
         conversation = run.outputs.get("chat_history")
-        return {"key": "conversation_lengthd" , "score": len(conversation)}
+        return {"key": "conversation_length" , "score": len(conversation)}
+
+
+
 
 class TasEvaluator:
     """Class for evaluating the Teaching Agent System."""
 
-    def __init__(self, eval_model):
+    def __init__(self, eval_model, experiment_name: str):
         """Initialize."""
         self.eval_model = eval_model
+        self.experiment_name = experiment_name
         self.eval_prompt = self.build_eval_prompt()
         self.eval_chain = self.build_eval_chain()
 
-
+    """Parses the dataset to extract the chat history which is evaluated upon."""
     def output_dataset(self, inputs: dict) -> dict:
         """Extract entire chat history from dataset."""
         chat_hist = inputs["chat_history"]
@@ -67,27 +59,40 @@ class TasEvaluator:
                 chat_string += f"Student: {message['content']}\n\n"
         return {"chat_history": chat_string}
 
-
-    """Prompt for the Teaching Agent System."""
+    """Prompt for the TAS Evaluator."""
     def build_eval_prompt(self):
         """Build the agent prompt."""
-        criteria = """Criteria 1: Correctness of the answer.\nCriteria 2: Clarity of the answer.\nCriteria 3: Relevance of the answer.\nCriteria 4: Engagement with the student."""
+        criteria = """Correctness: Correctness of the answer.\nCriteria 2: Clarity of the answer.\nCriteria 3: Relevance of the answer.\nCriteria 4: Engagement with the student."""
 
         prompt_hub_template = hub.pull("augustsemrau/react-teaching-chat-evaluator").template
         prompt_template = PromptTemplate.from_template(template=prompt_hub_template)
-        prompt = prompt_template.partial(criteriae=criteria)
+        prompt = prompt_template.partial(criteria=criteria)
         return prompt
 
     def build_eval_chain(self):
         """Build the evaluation chain."""
-        chain = self.eval_prompt | self.eval_model | output_parser.StrOutputParser()
+        chain = self.eval_prompt | self.eval_model | JsonOutputParser()#output_parser.StrOutputParser()
         return chain
 
     def run_eval_chain(self, run: Run, example: Example) -> dict:
         """Run the evaluation chain."""
         conversation = run.outputs.get("chat_history")
-        response = self.eval_chain.invoke({"input": conversation})
-        return response
+        output = self.eval_chain.invoke({"input": conversation})
+        print("Response:\n", output)
+        # criteria_grades = {}
+        # for criteria in output["Response"]:
+        #     print("Criteria:\n", criteria)
+        #     for key, value in criteria.items():
+        #         print({f"{key} Grade": value['Grade']})
+        #         criteria_grades[key] = value['Grade']
+        criteria_grades = []
+        for criteria in output:
+            print("Criteria:\n", criteria)
+            for key, value in criteria.items():
+                print({f"{key} Grade": value['Grade']})
+                criteria_grades.append({"key": str(key) , "score": float(value['Grade'])})
+        print("Criteria grades:\n", criteria_grades)
+        return criteria_grades
 
     def run_experiment(self, dataset_name):
         """Run the evaluation experiment."""
@@ -99,9 +104,9 @@ class TasEvaluator:
         experiment_results = evaluate(self.output_dataset,
                 data=dataset_name,
                 evaluators=evalulators,
-                experiment_prefix="test-dbrx-qa-custom-eval-is-answered",
+                experiment_prefix=self.experiment_name,
                 # Any experiment metadata can be specified here
-                metadata={"variant": "stuff website context into gpt-3.5-turbo",},
+                # metadata={"variant": "stuff website context into gpt-3.5-turbo",},
                 )
         return experiment_results
 
@@ -109,6 +114,9 @@ class TasEvaluator:
 
 
 if __name__ == "__main__":
-    langsmith_name = None#"Langsmith Eval Experiment 1"
+    langsmith_name = "Langsmith Eval Experiment 1"
     llm_model = init_llm_langsmith(llm_key=3, temp=0.5, langsmith_name=langsmith_name)
-    evaluator_class = TasEvaluator(eval_model=llm_model)
+    experiment_name = "TAS Evaluation Test 1"
+    evaluator_class = TasEvaluator(eval_model=llm_model, experiment_name=experiment_name)
+    dataset_name = "TAS v0 Conversations"
+    experiment_results = evaluator_class.run_experiment(dataset_name=dataset_name)
