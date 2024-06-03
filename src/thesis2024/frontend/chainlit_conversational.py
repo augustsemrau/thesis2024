63e288dfbd21057342b2183a7d8fdeb4cbe0da2d
August Semrau Andersen <augu0093@gmail.com>
2024-06-03 21:06:23 +0200

    frontend run reflexion

--- src/thesis2024/frontend/chainlit_conversational.py
+++ src/thesis2024/frontend/chainlit_conversational.py
@@ -2,55 +2,66 @@
 
 # To run (from thesis2024/src/thesis2024/)
 # chainlit run frontend/chainlit_conversational.py -w --port 7097
-import time
 import chainlit as cl
 from thesis2024.utils import init_llm_langsmith
 from thesis2024.TAS import TAS
+from thesis2024.multiagent_modules.reflexion_multiagent import ReflexionMultiAgent
 
 
-"""Chainlit testing of the TAS."""
-@cl.on_chat_start
-def main():
-    """Instantiate required classes for the user session."""
-    tas_version = "v1"
-    time_now = time.strftime("%Y.%m.%d-%H.%M.")
-    langsmith_name = tas_version + " Chainlit-Conversation " + time_now
-    llm_model = init_llm_langsmith(llm_key=40, temp=0.5, langsmith_name=langsmith_name)
-    tas = TAS(llm_model=llm_model,
-            version=tas_version,
-            course="DeepLearning",#"IntroToMachineLearning",
-            student_name="August",
-            student_id="AugustSemrau001"
-            )
-    cl.user_session.set("tas", tas)
-
-@cl.on_message
-async def main(message: str):
-    """Call the TAS asynchronously and send the response to the user."""
-    tas = cl.user_session.get("tas")
-    # response = tas.cl_predict(query=message.content)
-    res = await tas.tas_executor.ainvoke({"input": message.content,}, callbacks=[cl.AsyncLangchainCallbackHandler()])
-    response = res["output"]
-    await cl.Message(content=response).send()
-
-
-
-
-
-"""Chainlit testing of the SSA."""
+# """Chainlit testing of the TAS."""
 # @cl.on_chat_start
 # def main():
 #     """Instantiate required classes for the user session."""
-#     llm_model = init_llm_langsmith(llm_key=3, temp=0.5, langsmith_name="CHAINLIT SSA TEST 1")
-#     ssa = SSA(llm_model=llm_model)
-#     ssa_v0 = ssa.build_ssa_v0()
-#     cl.user_session.set("ssa", ssa_v0)
+#     student_name, student_course, student_subject = "August", "IntroToMachineLearning", "Linear Regression"
+#     student_learning_preferences = "I prefer formulas and math in order to understand technical concepts"
+
+#     """TAS Setup"""
+#     llm_model = init_llm_langsmith(llm_key=40, temp=0.5, langsmith_name="TAS Chainlit-Conversation")
+#     tas = TAS(llm_model=llm_model,
+#             baseline_bool=False,
+#             course=student_course,
+#             subject=student_subject,
+#             learning_prefs=student_learning_preferences,
+#             student_name=student_name,
+#             student_id="AugustSemrau97",
+#             )
+#     cl.user_session.set("tas", tas)
 
 # @cl.on_message
 # async def main(message: str):
 #     """Call the TAS asynchronously and send the response to the user."""
-#     ssa = cl.user_session.get("ssa")
-#     res = await ssa.ainvoke({"input": message.content,}, callbacks=[cl.AsyncLangchainCallbackHandler()])
-#     await cl.Message(content=res["text"]).send()
+#     tas = cl.user_session.get("tas")
+#     # response = tas.cl_predict(query=message.content)
+#     res = await tas.tas_executor.ainvoke({"input": message.content,}, callbacks=[cl.AsyncLangchainCallbackHandler()])
+#     response = res["output"]
+#     await cl.Message(content=response).send()
+
+
+"""Chainlit testing of Reflexion setup."""
+@cl.on_chat_start
+def main():
+    """Instantiate required classes for the user session."""
+    student_name, student_course, student_subject = "August", "IntroToMachineLearning", "Linear Regression"
+    student_learning_preferences = "I prefer formulas and math in order to understand technical concepts"
+
+    """Reflexion Setup"""
+    # reflexion_model = init_llm_langsmith(llm_key=3, temp=0.5, langsmith_name="REFLEXION TEST")
+    llm_model = init_llm_langsmith(llm_key=3, temp=0.5, langsmith_name="REFLEXION TEST")
+    # llm_model = init_llm_langsmith(llm_key=4, temp=0.5, langsmith_name="BASELINE_CHAIN")
+    reflexion = ReflexionMultiAgent(llm_model=llm_model,
+                                    reflexion_model=llm_model,
+                                    max_iter=1,
+                                    course=student_course,
+                                    )
+    cl.user_session.set("reflexion", reflexion)
+
+@cl.on_message
+async def main(message: str):
+    """Call the TAS asynchronously and send the response to the user."""
+    reflexion = cl.user_session.get("reflexion")
+    # response = tas.cl_predict(query=message.content)
+    res = await reflexion.reflexion_executor.ainvoke({"input": message.content,}, callbacks=[cl.AsyncLangchainCallbackHandler()])
+    response = res["output"]
+    await cl.Message(content=response).send()
 
 
